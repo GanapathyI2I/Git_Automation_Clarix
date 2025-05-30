20f20f0427e6cbe20db3294de0c17863044e4ddd
GanapathyI2I <ganapathy.radhakrishnan@ideas2it.com>
2025-05-30 20:09:31 +0530

    PayablesTest - Completed

---
+++ src/main/java/com/idrx/qa/pages/PayablesPage.java
@@ -0,0 +1,79 @@
+package com.idrx.qa.pages;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.PageFactory;
+
+import com.idrx.qa.base.TestBase;
+
+public class PayablesPage extends TestBase {
+
+    @FindBys({
+            @FindBy(xpath = "//span[contains(text(), 'Total Payables')]/ancestor::visual-container/..//visual-modern//*[local-name()='svg' and contains(@aria-label, 'Payable Total amount')]") })
+    List<WebElement> Payables;
+
+    @FindBys({
+            @FindBy(xpath = "//*[local-name()='svg' and starts-with(@aria-label, 'Pay_value')]//*[local-name()='tspan']") })
+    List<WebElement> pendingPayables;
+
+    @FindBys({
+            @FindBy(xpath = "//*[@class = \"labelContainerGraphicsContext\"]//*[@class=\"label-container\"]//*[@class=\"label-tspan\"]") })
+    List<WebElement> monthwisePayablesTrend;
+
+    public PayablesPage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    public String getTotalPayables() {
+        WebElement totalPayables = this.Payables.get(0);
+        return totalPayables.getText();
+    }
+
+    public String getSpares() {
+        WebElement spares = this.Payables.get(1);
+        return spares.getText();
+    }
+
+    public String getSales() {
+        WebElement sales = this.Payables.get(2);
+        return sales.getText();
+    }
+
+    public String getMahindra() {
+        WebElement mahindra = this.Payables.get(3);
+        return mahindra.getText();
+    }
+
+    public String getService() {
+        WebElement service = this.Payables.get(4);
+        return service.getText();
+    }
+
+    public String getOthers() {
+        WebElement others = this.Payables.get(5);
+        return others.getText();
+    }
+
+    public String getGreaterThan60DaysPendingPayables() {
+        WebElement greaterThan60DaysPendingPayables = this.pendingPayables.get(0);
+        return greaterThan60DaysPendingPayables.getText();
+    }
+
+    public String getGreaterThan30DaysPendingPayables() {
+        WebElement greaterThan30DaysPendingPayables = this.pendingPayables.get(1);
+        return greaterThan30DaysPendingPayables.getText();
+    }
+
+    public String currentMonthPayablesTrend() {
+        WebElement currentMonthPayablesTrend = this.monthwisePayablesTrend.get(1);
+        return currentMonthPayablesTrend.getText();
+    }
+
+    public String lastMonthPayablesTrend() {
+        WebElement lastMonthPayablesTrend = this.monthwisePayablesTrend.get(3);
+        return lastMonthPayablesTrend.getText();
+    }
+}
--- src/main/java/com/idrx/qa/util/TestUtil.java
+++ src/main/java/com/idrx/qa/util/TestUtil.java
@@ -17,12 +17,13 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.Assert;
 
 import com.idrx.qa.base.TestBase;
 
-public class TestUtil extends TestBase{
-	
+public class TestUtil extends TestBase {
+
 	public static final String EXCELFILELOCATION = "./src/main/java/Automation data.xlsx";
 	public static FileInputStream fis;
 	public static Workbook workbook;
@@ -30,89 +31,129 @@ public class TestUtil extends TestBase{
 	public static Row row;
 	public static DataFormatter dataformatter;
 	public final Logger logger = LogManager.getLogger(TestUtil.class);
-	
-	public static void switchToFrame() {		
+
+	public static void switchToFrame() {
 		driver.switchTo().frame(driver.findElement(By.tagName("iframe")));
 	}
-	
-	public static void loadExcel(String sheetName) throws Exception {		  
-		
+
+	public static void loadExcel(String sheetName) throws Exception {
+
 		try {
 			fis = new FileInputStream(EXCELFILELOCATION);
-			 workbook = new XSSFWorkbook(fis);
-			 sheet = workbook.getSheet(sheetName);
-		} catch (FileNotFoundException e) {			
+			workbook = new XSSFWorkbook(fis);
+			sheet = workbook.getSheet(sheetName);
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		} catch (IOException e) {			
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public static Map<String, Object> getExcelValue(String sheetName) throws Exception{
-		
-		if(sheet == null) {
+
+	public static Map<String, Object> getExcelValue(String sheetName) throws Exception {
+
+		if (sheet == null) {
 			loadExcel(sheetName);
 		}
-		
+
 		Map<String, Object> myMap = new HashMap<String, Object>();
-		
-		for(int i = 1; i<sheet.getLastRowNum() + 1; i++) {
+
+		for (int i = 1; i < sheet.getLastRowNum() + 1; i++) {
 			row = sheet.getRow(i);
 			Cell keyCell = row.getCell(0);
-			String key = keyCell.getStringCellValue().trim();			
+			String key = keyCell.getStringCellValue().trim();
 			Cell valueCell = row.getCell(1);
-			Object value = null;			
-			if(valueCell != null) {
-				if(valueCell.getCellType() == 0) {
-					value = valueCell.getNumericCellValue();					
-				} else if(valueCell.getCellType() == 1) {
+			Object value = null;
+			if (valueCell != null) {
+				if (valueCell.getCellType() == 0) {
+					value = valueCell.getNumericCellValue();
+				} else if (valueCell.getCellType() == 1) {
 					value = valueCell.getStringCellValue();
 				}
-			}							
-			myMap.put(key, value);			
+			}
+			myMap.put(key, value);
 		}
 		return myMap;
 	}
-	
+
 	public static Object getValue(String key, String sheetName) throws Exception {
 		Map<String, Object> value = getExcelValue(sheetName);
 		Object getValueByKey = value.get(key);
 		return getValueByKey;
 	}
-	
-	 public <T> void assertEquals(T actual, T expected, String message) {
-		 logger.info("Asserting values. Expected: [{}], Actual: [{}]", expected, actual);
-		 Assert.assertEquals(actual, expected, message);		 
-		 logger.info("Assertion passed: {}", message);		 
-	 }
-	 
-	 public void sendKeys(WebElement element, String value, String description) {
-	        try {
-	            logger.info("Entering Value in {} → '{}'", description, value);
-	            element.clear();
-	            element.sendKeys(value);
-	            logger.info("✅ Entered Value successfully in: {}", description);
-	        } catch (Exception e) {
-	            logger.error("❌ Failed to type in: {} - {}", description, e.getMessage());
-	            throw e;
-	        }
-	 }
-	 
-	 public void clickElement(WebElement element, String description) {
-	        try {
-	            logger.info("Clicking on: {}", description);
-	            element.click();
-	            logger.info("✅ Clicked successfully on: {}", description);
-	        } catch (Exception e) {
-	            logger.error("❌ Failed to click on: {} - {}", description, e.getMessage());
-	            throw e;
-	        }
-	}
-	 
-	 public void launchURL(WebDriver driver, String url) {
-	        logger.info("🌐 Launching URL: {}", url);
-	        driver.get(url);
-	        logger.info("✅ URL launched successfully");
-	 }
 
+	public <T> void assertEquals(T actual, T expected, String message) {
+		logger.info("Asserting values. Expected: [{}], Actual: [{}]", expected, actual);
+		Assert.assertEquals(actual, expected, message);
+		logger.info("Assertion passed: {}", message);
+	}
+
+	public void sendKeys(WebElement element, String value, String description) {
+		try {
+			logger.info("Entering Value in {} → '{}'", description, value);
+			element.clear();
+			element.sendKeys(value);
+			logger.info("✅ Entered Value successfully in: {}", description);
+		} catch (Exception e) {
+			logger.error("❌ Failed to type in: {} - {}", description, e.getMessage());
+			throw e;
+		}
+	}
+
+	public void clickElement(WebElement element, String description) {
+		try {
+			logger.info("Clicking on: {}", description);
+			element.click();
+			logger.info("✅ Clicked successfully on: {}", description);
+		} catch (Exception e) {
+			logger.error("❌ Failed to click on: {} - {}", description, e.getMessage());
+			throw e;
+		}
+	}
+
+	public void launchURL(WebDriver driver, String url) {
+		logger.info("🌐 Launching URL: {}", url);
+		driver.get(url);
+		logger.info("✅ URL launched successfully");
+	}
+
+	public static String numberToShortIndianFormat(double num) {
+		if (num >= 10000000) {
+			double value = num / 10000000.0;
+			String finalValueCr = String.format("%.2f Cr", value).replaceAll("\\.?0+Cr$", "Cr");
+			System.out.println("finalValueCr: " + finalValueCr);
+			return finalValueCr;
+		} else if (num >= 100000) {
+			double value = num / 100000.0;
+			String finalValueL = String.format("%.2f L", value).replaceAll("\\.?0+L$", "L");
+			System.out.println("finalValueL: " + finalValueL);
+			return finalValueL;
+		} else if (num >= 1000) {
+			double value = num / 1000.0;
+			String finalValueK = String.format("%.2f k", value).replaceAll("\\.?0+k$", "k");
+			System.out.println("finalValueK: " + finalValueK);
+			return finalValueK;
+		} else {
+			return String.valueOf(num);
+		}
+	}
+
+	/**
+	 * Scrolls down the page to the bottom using JavaScriptExecutor.
+	 * 
+	 * @param driver The WebDriver instance.
+	 */
+	public static void scrollToBottom(WebDriver driver) {
+		((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight);");
+	}
+
+	/**
+	 * Scrolls down the page by a specific number of pixels using
+	 * JavaScriptExecutor.
+	 * 
+	 * @param driver The WebDriver instance.
+	 * @param pixels The number of pixels to scroll down.
+	 */
+	public static void scrollDownBy(WebDriver driver, int pixels) {
+		((JavascriptExecutor) driver).executeScript("window.scrollBy(0, arguments[0]);", pixels);
+	}
 }
